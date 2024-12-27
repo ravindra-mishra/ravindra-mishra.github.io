cda07f5b788adcbc411bc72ab19844c5297cd5cb
Ravindra Mishra <ravindra.mishra@perficient.com>
2024-12-27 17:24:48 +0530

    adding env

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")], // Adjust the path to your styles folder as needed
   },
+  env: {
+    NEXT_PUBLIC_COMMENTBOX_PROJECT_ID:
+      process.env.NEXT_PUBLIC_COMMENTBOX_PROJECT_ID,
+  },
   basePath: "", // Replace with the name of your GitHub repository
   assetPrefix: "", // Same as the basePath
   trailingSlash: false,
